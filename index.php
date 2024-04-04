353b334f4187b6e028f08d00bfa353b02f3c5ee9
Willan Theunissen <willan@webvelopment.co.za>
2024-04-04 09:58:53 +0200

    init

---
+++ index.php
@@ -0,0 +1,256 @@
+<?php 
+/* ------------------------------------------------------ */ 
+
+// Question 1:
+// A job requirement is that you have 3 years of PHP experience, do you have this experience? 
+// What is the core difference in your opinion between something like a Wordpress site and a site built with Nuxt/React/Svelte/Vue?
+// What is jQuery
+// What is Bootstrap
+// What is SCSS
+// What is a CDN
+// What is a REST API
+// What is a CMS
+// What is Wordpress
+// In Wordpress, what is the primary data type and how does data get added to that type? 
+// What is understrap and ACF in Wordpress
+// What is the concept behind a "headless" CMS? Is Wordpress a headless CMS?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ------------------------------------------------------ */ 
+
+// Question 2:
+// What is the difference between "==" and "===" in PHP?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ------------------------------------------------------ */ 
+
+// Question 3: 
+// Explain at a very high level, what this code is?
+$foo = [];
+$foo['bar'] = 'baz';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ------------------------------------------------------ */ 
+
+// Question 4: 
+// Is there a difference for each of the following
+foreach($row as $column => $value) {
+    echo $key . ' => ' . $value;
+}
+// vs 
+foreach($row as $column => $value) :
+    print_r([$key, $value]);
+endforeach;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ------------------------------------------------------ */ 
+
+// Challenge 1: Output all items in the below array with a line break
+$array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ------------------------------------------------------ */ 
+
+// Challenge 2: Sort out all items, and output the array in ascending order
+$array = [7, 8, 9, 10, 1, 2, 3, 4, 5, 6];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ------------------------------------------------------ */ 
+
+// Challenge 3: Count from -10 to 100, echo eacho number
+
+
+
+
+
+/* ------------------------------------------------------ */
+
+// Challenge 4: Interpret a remote source
+// API Source: https://api.tvmaze.com/shows
+// Return the above as a workable array in PHP, and output the first 5 shows
+
+
+
+
+
+
+
+
+/* ------------------------------------------------------ */
+// Challenge 5: Using bootstrap, create a simple header and all shows in a grid that tagged as "Action" AND "Horror". Also include the shows image in a responsive 1:1 box Serve it via localhost
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
